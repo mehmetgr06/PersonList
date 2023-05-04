4049a461f66d71fa825c718400f9265744338ccf
mehmet_gur <mehmet.gur@getir.com>
2023-05-05 02:11:58 +0300

    mainViewModel added

---
+++ app/src/main/java/com/example/personlist/common/BaseViewModel.kt
@@ -0,0 +1,6 @@
+package com.example.personlist.common
+
+import androidx.lifecycle.ViewModel
+
+abstract class BaseViewModel : ViewModel() {
+}
---
+++ app/src/main/java/com/example/personlist/ui/MainViewModel.kt
@@ -0,0 +1,30 @@
+package com.example.personlist.ui
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.viewModelScope
+import androidx.paging.PagingData
+import androidx.paging.cachedIn
+import com.example.personlist.common.BaseViewModel
+import com.example.personlist.data.source.Person
+import com.example.personlist.domain.GetPersonsUseCase
+import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.flow.collectLatest
+import kotlinx.coroutines.launch
+import javax.inject.Inject
+
+@HiltViewModel
+class MainViewModel @Inject constructor(private val getPersonsUseCase: GetPersonsUseCase) :
+    BaseViewModel() {
+
+    private val _personsLiveData: MutableLiveData<PagingData<Person>> = MutableLiveData()
+    val personsLiveData: LiveData<PagingData<Person>> = _personsLiveData
+
+    fun getTrainers() {
+        viewModelScope.launch {
+            getPersonsUseCase().cachedIn(this).collectLatest { personsData ->
+                _personsLiveData.value = personsData
+            }
+        }
+    }
+}
